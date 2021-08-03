998f68ce54e1ecf9744acad1166ba6e8acce02c5
amina62 <amina.sohail62@gmail.com>
2021-08-03 18:22:28 +0100

    second commit

--- musicapp/urls.py
+++ musicapp/urls.py
@@ -3,6 +3,27 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+
+    #class based views urls:
+    path('artist/', views.ArtistListView.as_view(), name='artist-list'),
+    path('artist-create/', views.ArtistCreateView.as_view(), name='artist-create'),
+    path('artist/<int:pk>', views.ArtistDetailView.as_view(), name='artist-detail'),
+    path('artist/update/<int:pk>', views.ArtistUpdateView.as_view(), name='artist-update'),
+    path('artist/delete/<int:pk>', views.ArtistDeleteView.as_view(), name='artist-delete'),
+
+    path('album/', views.AlbumListView.as_view(), name='album-list'),
+    path('album-create/', views.AlbumCreateView.as_view(), name='album-create'),
+    path('album/<int:pk>', views.AlbumDetailView.as_view(), name='album-detail'),
+    path('album/update/<int:pk>', views.AlbumUpdateView.as_view(), name='album-update'),
+    path('album/delete/<int:pk>', views.AlbumDeleteView.as_view(), name='album-delete'),
+
+    path('song/', views.SongsListView.as_view(), name='song-list'),
+    path('song-create/', views.SongCreateView.as_view(), name='song-create'),
+    path('song/<int:pk>', views.SongDetailView.as_view(), name='song-detail'),
+    path('song/update/<int:pk>', views.SongUpdateView.as_view(), name='song-update'),
+    path('song/delete/<int:pk>', views.SongDeleteView.as_view(), name='song-delete'),
+
+    #functial based views urls:
     path('add_artist/', views.create_artist, name='create-artist'),
     path('update_artist/<str:pk>', views.update_artist, name='update-artist'),
     path('delete_artist/<str:pk>', views.delete_artist, name='delete-artist'),
--- musicapp/views.py
+++ musicapp/views.py
@@ -1,11 +1,111 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import ArtistForm, AlbumForm, SongForm
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 from .models import *
 
+#class based views:
+
+# Artists
+main_html = "list.html"
+
+class ArtistListView(ListView):
+    model = Artist
+    template_name = main_html
+    context_object_name = 'artists'
+
+class ArtistCreateView(CreateView):
+    model = Artist
+    template_name = 'artist_create.html'
+    # template_name = main_html
+    fields = ['name', ]
+    success_url = reverse_lazy('artist-list')
+
+class ArtistDetailView(DetailView):
+    model = Artist
+    template_name = 'artist_detail.html'
+    context_object_name = 'artists'
+
+class ArtistUpdateView(UpdateView):
+    model = Artist
+    template_name = 'artist_update.html'
+    context_object_name = 'artists'
+    fields = ['name', ]
+    def get_success_url(self):
+        return reverse_lazy('artist-detail', kwargs={'pk': self.object.id})
+
+class ArtistDeleteView(DeleteView):
+    model = Artist
+    template_name = 'artist_delete.html'
+    success_url = reverse_lazy('artist-list')
+
+# Albums
+class AlbumListView(ListView):
+    model = Album
+    template_name = 'album_list.html'
+    context_object_name = 'albums'
+
+class AlbumCreateView(CreateView):
+    model = Album
+    template_name = 'album_create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('album-list')
+
+class AlbumDetailView(DetailView):
+    model = Album
+    template_name = 'album_detail.html'
+    context_object_name = 'albums'
+
+class AlbumUpdateView(UpdateView):
+    model = Album
+    template_name = 'album_update.html'
+    context_object_name = 'albums'
+    fields = '__all__'
+    def get_success_url(self):
+        return reverse_lazy('album-detail', kwargs={'pk': self.object.id})
+
+class AlbumDeleteView(DeleteView):
+    model = Album
+    template_name = 'album_delete.html'
+    success_url = reverse_lazy('album-list')
+
+
+# Songs
+class SongsListView(ListView):
+    model = Song
+    template_name = 'song_list.html'
+    context_object_name = 'songs'
+
+class SongCreateView(CreateView):
+    model = Song
+    template_name = 'song_create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('song-list')
+
+class SongDetailView(DetailView):
+    model = Song
+    template_name = 'song_detail.html'
+    context_object_name = 'songs'
+
+class SongUpdateView(UpdateView):
+    model = Song
+    template_name = 'song_update.html'
+    context_object_name = 'songs'
+    fields = '__all__'
+    def get_success_url(self):
+        return reverse_lazy('song-detail', kwargs={'pk': self.object.id})
+
+class SongDeleteView(DeleteView):
+    model = Song
+    template_name = 'song_delete.html'
+    success_url = reverse_lazy('song-list')
+
+
+#function based views:
 def home(request):
     artists = Artist.objects.all()
     albums = Album.objects.all()
